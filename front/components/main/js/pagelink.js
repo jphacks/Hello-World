0eed6126ce10342baa88ac17c05fd8e63f505170
Yuta_O <yutakafumimario527@gmail.com>
2016-11-04 19:53:24 +0900

    js files

---
+++ front/components/main/js/pagelink.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('a[href^=#]').click(function(){
+        var speed = 500; //移動完了までの時間(sec)を指定
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
