32dd4f8a0991f912d73e86ed9dd7bfda5e807564
Tamrin007 <tamrin.bitterchococo@gmail.com>
2016-11-05 14:15:16 +0900

    コード実行コンテナを起動するコード

--- back/controller/executor.go
+++ back/controller/executor.go
@@ -1,9 +1,13 @@
 package controller
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 
+	"github.com/mattn/go-shellwords"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
@@ -23,6 +27,10 @@ func (e *Executor) Handle(c *gin.Context) {
 	c.BindJSON(&request)
 	fmt.Println(request.Language)
 	var response Result
+	response, err := request.Exec(request)
+	if err != nil {
+		fmt.Println("ERROR!!!: ", err)
+	}
 
 	c.String(http.StatusOK, "%s", "ok")
 	c.JSON(http.StatusOK, gin.H{
@@ -32,17 +40,149 @@ func (e *Executor) Handle(c *gin.Context) {
 }
 
 func (e *Executor) Exec(request Executor) (Result, error) {
-	var exec_result Result
+	var execResult Result
+	var (
+		filename string
+		execCmd  string
+	)
 	switch request.Language {
 	case "ruby":
-		exec_result.RunTime = "0,04ms"
-		exec_result.Output = "Hello world"
+		filename = "Main.rb"
+		execCmd = "ruby" + filename
 	case "js":
-		exec_result.RunTime = "0,04ms"
-		exec_result.Output = "Hello world"
+		filename = "script.js"
+		execCmd = "node" + filename
 	default:
-		exec_result.Output = "Error"
-		exec_result.RunTime = "-ms"
+		execResult.Output = "Invalid language"
+		execResult.RunTime = "-ms"
 	}
-	return exec_result, nil
+
+	output, err := createDocker(execCmd)
+	if err != nil {
+		return execResult, nil
+	}
+
+	// get container id from output
+	containerID := output[0:12]
+	fmt.Println("container ID: ", containerID)
+
+	// コンテナにユーザコードをコピー
+	err = copyToContainer(request.Code, filename, containerID)
+	if err != nil {
+		return execResult, err
+	}
+
+	// ユーザコードの実行
+	execResult.Output, execResult.RunTime, err = startContainer(containerID)
+	if err != nil {
+		return execResult, err
+	}
+
+	err = removeContainer(containerID)
+	if err != nil {
+		return execResult, err
+	}
+
+	return execResult, nil
+}
+
+func createDocker(execCmd string) (string, error) {
+	// create docker image
+	dockerCmd :=
+		`docker create -i ` +
+			`--net none ` +
+			`--cpuset-cpus 0 ` +
+			`--memory 512m --memory-swap 512m ` +
+			`--ulimit nproc=10:10 ` +
+			`--ulimit fsize=1000000 ` +
+			`-w /workspace ` +
+			`exec-container ` +
+			`/user/bin/time -q -f "%e" -o /time.txt ` +
+			`timeout 3 ` +
+			`su nobody -s /bin/bash -c "` +
+			execCmd +
+			`"`
+
+	fmt.Println("exec: ", dockerCmd)
+	cmd, err := shellwords.Parse(dockerCmd)
+	if err != nil {
+		return "", err
+	}
+	output, err := exec.Command(cmd[0], cmd[1:]...).Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(output), err
+}
+
+func copyToContainer(code string, filename string, containerID string) error {
+	exec.Command("rm", "-rf", "/tmp/workspace").Run()
+	err := exec.Command("mkdir", "-rf", "/tmp/workspace").Run()
+	if err != nil {
+		return err
+	}
+	err = exec.Command("chmod", "777", "/tmp/workspace").Run()
+	if err != nil {
+		return err
+	}
+	dockerCmd := `docker cp /tmp/workspace ` + containerID + ":/"
+	fmt.Println("exec: ", dockerCmd)
+	cmd, err := shellwords.Parse(dockerCmd)
+	if err != nil {
+		return err
+	}
+	err = exec.Command(cmd[0], cmd[1:]...).Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func startContainer(containerID string) (string, string, error) {
+	dockerCmd := `docker start -i ` + containerID
+	fmt.Println("exec: ", dockerCmd)
+	cmd, err := shellwords.Parse(dockerCmd)
+	if err != nil {
+		return "", "", err
+	}
+	output, err := exec.Command(cmd[0], cmd[1:]...).Output()
+	if err != nil {
+		return "", "", err
+	}
+	dockerCmd = `docker cp ` + containerID + `:/time.txt /tmp/time.txt`
+	fmt.Println("exec: ", dockerCmd)
+	cmd, err = shellwords.Parse(dockerCmd)
+	if err != nil {
+		return "", "", err
+	}
+	err = exec.Command(cmd[0], cmd[1:]...).Run()
+	if err != nil {
+		return "", "", err
+	}
+	fp, err := os.Open("/tmp/time.txt")
+	scanner := bufio.NewScanner(fp)
+	var time string
+	if scanner.Scan() {
+		time = scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		return "", "", err
+	}
+	return string(output), time, nil
+}
+
+func removeContainer(containerID string) error {
+	dockerCmd := `docker rm ` + containerID
+	fmt.Println("exec: ", dockerCmd)
+	cmd, err := shellwords.Parse(dockerCmd)
+	if err != nil {
+		return err
+	}
+	err = exec.Command(cmd[0], cmd[1:]...).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
