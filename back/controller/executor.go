86626630d66921df62ea59d9da4fd0764970a1e3
Tamrin007 <tamrin.bitterchococo@gmail.com>
2016-11-05 11:51:29 +0900

    create Exec()

--- back/controller/executor.go
+++ back/controller/executor.go
@@ -19,25 +19,30 @@ type Result struct {
 }
 
 func (e *Executor) Handle(c *gin.Context) {
-	var exec Executor
-	c.BindJSON(&exec)
-	fmt.Println(exec.Language)
-	var result Result
-	switch exec.Language {
-	case "ruby":
-		result.RunTime = "0,04ms"
-		result.Output = "Hello world"
-	case "js":
-		result.RunTime = "0,04ms"
-		result.Output = "Hello world"
-	default:
-		result.Output = "Error"
-		result.RunTime = "-ms"
-	}
+	var request Executor
+	c.BindJSON(&request)
+	fmt.Println(request.Language)
+	var response Result
 
 	c.String(http.StatusOK, "%s", "ok")
 	c.JSON(http.StatusOK, gin.H{
-		"run_time": result.RunTime,
-		"output":   result.Output,
+		"run_time": response.RunTime,
+		"output":   response.Output,
 	})
+}
+
+func (e *Executor) Exec(request Executor) (Result, error) {
+	var exec_result Result
+	switch request.Language {
+	case "ruby":
+		exec_result.RunTime = "0,04ms"
+		exec_result.Output = "Hello world"
+	case "js":
+		exec_result.RunTime = "0,04ms"
+		exec_result.Output = "Hello world"
+	default:
+		exec_result.Output = "Error"
+		exec_result.RunTime = "-ms"
+	}
+	return exec_result, nil
 }
