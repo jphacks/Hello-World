06fbd4ccbdc55d392aec95f34a486d3b45341303
kdrl <kdrl7@naver.com>
2016-11-15 00:03:29 +0900

    prevent howling 4th try

--- front/components/room/room.controller.js
+++ front/components/room/room.controller.js
@@ -117,11 +117,10 @@ export default class roomController {
                 angular.element('.videos')[0].style.height = angular.element(window).height()+"px";
             });
             angular.element('.videos').append(
-                '<div class="videoBox video_' + peerId + '"><video id="myVideo video_' + peerId + '" class="remoteVideos" width="100%" autoplay="autoplay" src="' + streamURL + '" > </video></div>'
+                '<div class="videoBox video_' + peerId + '"><video id="myVideo video_' + peerId + '" class="remoteVideos" width="100%" volume="0" autoplay="autoplay" src="' + streamURL + '" > </video></div>'
             );
             //prevent howling
-            console.log("prevent howling!")
-            angular.element('myVideo').prop('muted', true);
+            console.log("prevent howling!!")
             /*
             自分のvideoを表示できてから、roomに入る準備をする。
             ここでthis.roomNameが入ろうとするroomを特定するkeyとなる。
